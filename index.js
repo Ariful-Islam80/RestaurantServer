f0ee87fdd8ea5511819b10c8267fce1c2c919bf3
ariful-islam <miarif4321@gmail.com>
2023-12-21 18:27:28 +0600

    I added pagination in my project

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(process.env.DB_PASS);
 
 
 // middleware
@@ -28,12 +27,42 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('BanglaRestaurant').collection('foods');
+    app.get('/foods', async (req, res) => {
+      const page = parseInt(req.query.page);
+      const skip = parseInt(req.query.skip);
+      console.log(page, skip);
+      const result = await foodCollection.find()
+        .skip(page * skip)
+        .limit(skip)
+        .toArray();
+      res.send(result);
+    })
+
+    app.get('/foodsCount', async (req, res) => {
+      const count = await foodCollection.estimatedDocumentCount();
+      res.send({ count })
+    })
+
+    app.get('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { name: 1, img: 1, category: 1, price: 1, country: 1, description: 1 }
+      }
+
+      const result = await foodCollection.findOne(query, options);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -42,9 +71,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Bangla RestaurantServer is Running')
+  res.send('Bangla RestaurantServer is Running')
 });
 
 app.listen(port, () => {
-    console.log(`Bangla Restaurant Server is Running${port}`);
+  console.log(`Bangla Restaurant Server is Running${port}`);
 });
