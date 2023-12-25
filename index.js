825b5df2db9c7ec7935990b3f27acfd5ffad6029
ariful-islam <miarif4321@gmail.com>
2023-12-25 21:43:50 +0600

    I apply to added card and delete functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser=require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -8,7 +10,14 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+})
+);
+app.use(cookieParser())
 app.use(express.json());
 
 
@@ -26,24 +35,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    // foods services and pagination
 
     const foodCollection = client.db('BanglaRestaurant').collection('foods');
     app.get('/foods', async (req, res) => {
       const page = parseInt(req.query.page);
       const skip = parseInt(req.query.skip);
-      console.log(page, skip);
+      // console.log(page, skip);
       const result = await foodCollection.find()
         .skip(page * skip)
         .limit(skip)
         .toArray();
+      // console.log(result);
       res.send(result);
     })
-
+    // for pagination products total count.
     app.get('/foodsCount', async (req, res) => {
       const count = await foodCollection.estimatedDocumentCount();
       res.send({ count })
     })
+
+    // specific data loaded and show
 
     app.get('/foods/:id', async (req, res) => {
       const id = req.params.id;
@@ -51,9 +65,57 @@ async function run() {
       const options = {
         projection: { name: 1, img: 1, category: 1, price: 1, country: 1, description: 1 }
       }
-
       const result = await foodCollection.findOne(query, options);
       res.send(result)
+    })
+
+    // foodCart api
+
+    app.get('/addCart', async (req, res) => {
+      const cursor = foodCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+
+    })
+
+    app.post('/addCart', async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await foodCollection.insertOne(data)
+      res.send(result)
+    })
+
+    // my added cart delete
+    app.delete('/addCart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await foodCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'none'
+        // httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
+        // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+        .send({ success: true });
+
+    })
+    // cookie deleted api
+    app.post('/logOut', (req, res) => {
+      const user = req.body;
+      console.log('login out', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
 
 
