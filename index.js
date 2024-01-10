3018d6e4ebe708d5fb58dc102dd889a22677eec9
RF-Rifat <rfrifat6344@gmail.com>
2024-01-11 02:44:45 +0600

    search functionallity updated by backend code

--- index.js
+++ index.js
@@ -20,25 +20,24 @@ app.use(express.json());
 // middlewares
 const logger = (req, res, next) => {
   console.log("log info", req.method);
-  next()
-}
+  next();
+};
 
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
-  console.log('token in the middleware', token);
+  console.log("token in the middleware", token);
   // no token available
   if (!token) {
-    return res.status(401).send({ message: "unauthorized access" })
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "unauthorized access" })
+      return res.status(401).send({ message: "unauthorized access" });
     }
     req.user = decoded;
     next();
-
-  })
-}
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqj5bqq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -60,11 +59,18 @@ async function run() {
 
     const foodCollection = client.db("BanglaRestaurant").collection("foods");
     app.get("/foods", async (req, res) => {
+      const search = req.query.search;
+
+      let query = {};
+
+      if (search) {
+        query.name = { $regex: new RegExp(search, "i") };
+      }
       const page = parseInt(req.query.page);
       const skip = parseInt(req.query.skip);
 
       const result = await foodCollection
-        .find()
+        .find(query)
         .skip(page * skip)
         .limit(skip)
         .toArray();
@@ -114,7 +120,7 @@ async function run() {
       const { email } = req.query;
       console.log("token owner info", req?.user);
       if (req.user.email !== req.query.email) {
-        return res.status(403).send({message:"forbidden access"})
+        return res.status(403).send({ message: "forbidden access" });
       }
 
       if (!email) {
@@ -153,14 +159,17 @@ async function run() {
       };
 
       try {
-        const result = await foodCollection.updateOne(filter, updatedFoods, option);
+        const result = await foodCollection.updateOne(
+          filter,
+          updatedFoods,
+          option
+        );
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
-
 
     // add foods api
     const AddFoodsCollection = client
@@ -195,8 +204,8 @@ async function run() {
           // secure: false,
           // sameSite: "none",
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
