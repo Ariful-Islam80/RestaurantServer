2c170e23efeafe74b397342ff01fa535fb96bbde
ariful-islam <miarif4321@gmail.com>
2024-01-10 23:11:07 +0600

    deleted functionality added

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ async function run() {
       const user = req.body;
 
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1s",
+        expiresIn: "1h",
       });
 
       res
